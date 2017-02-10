3e164f055c053a1ec799bbbf09f23097f9c97531
Andrey Glazunov <Gvinar@yandex.ru>
2017-02-10 16:08:55 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+var Promise = require('bluebird');
+var request = require('request');
+var phantom = require('phantom');
+var phantomPath = require('phantomjs').path;
+
+var phInstance;
+var phPage;
+
+phantom.create([], { phantomPath: phantomPath })
+    .then(instance => {
+        phInstance = instance;
+        return instance.createPage();
+    }).then(page => {
+        phPage = page;
+
+        page.setting("userAgent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36");
+        page.setting("javascriptEnabled", true);
+        page.setting("loadImages", true);
+
+        page.on("onResourceRequested", function(requestData) {
+            console.info('Requesting', requestData.url)
+        });
+
+        return page.open('https://oauth.vk.com/authorize?client_id=3682744&display=wap&redirect_uri=oauth.vk.com/blank.html&scope=wall,offline&response_type=token&v=5.62');
+    }).then(function(status){
+        console.log(status);
+
+        return phPage.property('content');
+    }).then(function(content){
+        console.log(content);
+
+        setTimeout(() => {
+            phPage.evaluate(function(){
+                return document.getElementsByTagName('body');
+            }).then(html => {
+                console.log(html[0].innerHTML);
+                process.exit(0);
+            });
+        }, 2000);
+    }).catch(error => {
+    console.log(error);
+    phInstance.exit();
+    process.exit(1);
+});
+    //const page = await instance.createPage();
+    //await page.on("onResourceRequested", function(requestData) {
+    //    console.info('Requesting', requestData.url)
+    //});
+    //
+    //const status = await page.open('https://stackoverflow.com/');
+    //console.log(status);
+    //
+    //const content = await page.property('content');
+    //console.log(content);
+
+    //await instance.exit();
+
+//
+//var casper = require('casper').create();
+//var x = require('casper').selectXPath;
+//
+//casper.userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0');
